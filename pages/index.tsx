efcd3d825375a20510c1d56d930df11928273fc3
Marothi Mahlake <code_artist@outlook.com>
2022-06-19 16:04:45 +0200

    index.tsx makes database calls.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { getSession, signIn, signOut } from "next-auth/react";
 
-const Home: NextPage = ({ session }) => {
+import { prisma } from "../lib/prisma";
+
+const Home: NextPage = ({ profile, session }) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -33,7 +35,15 @@ export default Home;
 export const getServerSideProps = async (context) => {
   const session = await getSession(context);
 
+  if (!session) {
+    return {
+      props: { session: null },
+    };
+  }
+
+  const profile = await prisma.profile.findUnique({ where: { email: session?.user?.email } });
+
   return {
-    props: { session },
+    props: { profile, session },
   };
 };
