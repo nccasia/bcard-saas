6b5a3c4985432f00fd777216dd944f8ca3fcc66b
Thế <tienthe2973@gmail.com>
2023-03-22 18:26:27 +0700

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8d04c4a8b63d74db"; }
+/******/ 		__webpack_require__.h = function() { return "3117a6eabd31ec26"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
