ff3221f43a833309456beb0566cca191f975aaab
dat2 <dat.lethanh2@ncc.asia>
2023-03-28 18:01:30 +0700

    create konva card

---
+++ utils/konvaCard.ts
@@ -0,0 +1,53 @@
+
+
+export const editTextCard = (e: any) => {
+    const stage = e.target.getStage();
+    const node = e.target as any;
+    const textPosition = node.getAbsolutePosition();
+    const stageRect = stage.container().getBoundingClientRect();
+    const nodeRect = node.getClientRect();
+    const absPos = {
+        x: stageRect.left + nodeRect.x,
+        y: stageRect.top + nodeRect.y,
+    };
+    const textarea = document.createElement('input');
+    textarea.value = node.text();
+    document.body.appendChild(textarea);
+    textarea.style.position = 'absolute';
+    textarea.style.top = absPos.y + 'px';
+    textarea.style.left = absPos.x + 'px';
+    textarea.style.width = String(Number(nodeRect.width+20)) + 'px';
+    textarea.style.height = String(Number(nodeRect.height+20)) + 'px';
+    textarea.style.border = '1px solid red';
+    textarea.style.padding = '0';
+    textarea.style.fontSize = node.fontSize() + 'px';
+    textarea.style.fontFamily = node.fontFamily();
+    textarea.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
+    textarea.style.color = 'black';
+    textarea.style.resize = 'none';
+    textarea.focus();
+
+    // const handleTextareaBlur = () => {
+    //     if (!document.body.contains(textarea)) {
+    //         return;
+    //     }
+    //     const updatedText = textarea.value;
+    //     node.text(updatedText);
+    //     document.body.removeChild(textarea);
+    //     stage.batchDraw(); 
+    // };
+    // textarea.addEventListener('blur', handleTextareaBlur);   
+    return new Promise((resolve, reject) => {
+        const handleTextareaBlur = () => {
+            if (!document.body.contains(textarea)) {
+                reject(new Error('Textarea not found in document body.'));
+            }
+            const updatedText = textarea.value;
+            node.text(updatedText);
+            document.body.removeChild(textarea);
+            stage.batchDraw(); 
+            resolve(updatedText);
+        };
+        textarea.addEventListener('blur', handleTextareaBlur); 
+    });    
+};
