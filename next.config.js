02de5a60c2cb5cb4a5a207b211ea46e24d721043
Thế Lê <77580681+Theit284@users.noreply.github.com>
2023-04-05 13:48:21 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    GOOGLE_CLIENT_ID: "58205235959-fi4gjo8ts43ot6e66rsiba4rsf3q02u0.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
-    BASE_URL: "http://172.16.100.114:8080/",
-  },
-};
