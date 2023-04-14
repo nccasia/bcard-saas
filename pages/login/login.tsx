fd338112d5956ff7de1f4ac5c306a5f6caa9f2e2
Thế <tienthe2973@gmail.com>
2023-04-14 17:03:12 +0700

    fix login

--- pages/login/login.tsx
+++ pages/login/login.tsx
@@ -1,8 +1,6 @@
 import Head from "next/head";
 
-import { keyAdmin } from "../../admin/keyAdmin";
 import Login from "../../components/login/Login";
-import { prisma } from "../../lib/prisma";
 
 export default function login() {
   return (
@@ -14,14 +12,3 @@ export default function login() {
     </div>
   );
 }
-
-export const getServerSideProps = async () => {
-  const admin = await prisma.admin.createMany({
-    data: keyAdmin,
-    skipDuplicates: true,
-  });
-  console.log(prisma.profile);
-  return {
-    props: { admin },
-  };
-};
