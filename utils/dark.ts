bf8edf9b65715137262408219f7eb10a4b67c448
Thế <tienthe2973@gmail.com>
2023-04-24 15:28:59 +0700

    add sort nameid

--- utils/dark.ts
+++
@@ -1,9 +0,0 @@
-export function isDarkMode() {
-  if (typeof window === "undefined") {
-    return false;
-  }
-
-  const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
-
-  return darkModeQuery.matches;
-}
